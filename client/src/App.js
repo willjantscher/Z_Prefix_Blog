95fab590b501f14dad2db168732aafc9f975001d
willjantscher <william.jantscher@gmail.com>
2022-01-10 12:58:11 -0700

    added registration connection to backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
+import { user } from 'pg/lib/defaults';
+
+//npm run start to execute
+//in client, npm install axios
 
 function App() {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const register = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      firstName: firstName, 
+      lastName: lastName, 
+      username: username, 
+      password: password}).then(() => alert("successful registration"))
+  };
+
   return (
-    <div className="App">
+    <div className="App"> 
       <div className="registration">
         <h1>Registration</h1>
+
+        <label>First Name</label>
+        <input type="text" name="firstName" onChange={(e) => {
+          setFirstName(e.target.value)
+        }}/>
+
+        <label>Last Name</label>
+        <input type="text" name="lastName" onChange={(e) => {
+          setLastName(e.target.value)
+        }}/>
+
         <label>Username</label>
-        <input type="text" />
+        <input type="text" name="username" onChange={(e) => {
+          setUsername(e.target.value)
+        }}/>
+
         <label>Password</label>
-        <input type="text" />
-        <button> Register </button>
+        <input type="text" name="password" onChange={(e) => {
+          setPassword(e.target.value)
+        }}/>
+
+        <button onClick={register}> Register </button>
       </div>
+
       <div className="login">
-        <h1>Registration</h1>
+        <h1>Login</h1>
         <input type="text" placeholder="Username..." />
         <input type="password" placeholder="Password..." />
-        <button> Register </button>
+        <button> Login </button>
       </div>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,6 +1,21 @@
-const express = require('express');
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require ("cors")
 const app = express();
+const mysql = require("mysql");
+const { password } = require("pg/lib/defaults");
 const port = 3001;
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "re5202lo",
+    database: "blogDb",
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(port, () => {
       console.log(`server is running on port ${port}`)
@@ -8,13 +23,32 @@ app.listen(port, () => {
 //node server.js to run server
 //npm run devStart to use nodemon
 
-// app.get('/', (req, res) => {
-//     res.send('hello there')
-// })
 
 
 
+app.post('/api/insert', (req, res) => {
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const username = req.body.username
+    const password = req.body.password
 
+    const sqlInsert = "INSERT INTO users (firstName, lastName, username, password) VALUES (?,?,?,?)"
+    db.query(sqlInsert, [firstName, lastName, username, password], (err, res) =>{
+        console.log(username + " registered");
+    })
+});
+
+
+app.get('/', (req, res) => {})
+
+
+
+// root@localhost
+// 3g6lh?Pwwd(z
+
+
+
+// CREATE SCHEMA IF NOT EXISTS `blogdb` 
 
 // CREATE TABLE IF NOT EXISTS `new_schema1`.`Posts` (
 //     `id` INT NOT NULL AUTO_INCREMENT,
