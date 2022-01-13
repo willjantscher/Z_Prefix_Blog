95619d8153d829bda14eceff480ab79e3981d4dd
willjantscher <william.jantscher@gmail.com>
2022-01-12 23:17:40 -0700

    changed ports front end senging to

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
     const path = require('path');
     app.get('*', (req, res) => {
-      req.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 //http://localhost:3000
